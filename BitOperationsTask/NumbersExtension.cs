66bc188901a7cce9c8f9fcfabeea7c234c240c19
Farhad <epam@farhad.su>
2021-02-03 21:02:35 +0300

    All done!

--- BitOperationsTask/NumbersExtension.cs
+++ BitOperationsTask/NumbersExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BitOperationsTask
 {
@@ -26,7 +27,42 @@ namespace BitOperationsTask
         /// </example>
         public static int InsertNumberIntoAnother(int destinationNumber, int sourceNumber, int i, int j)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (i < 0 || i > 31)
+            {
+                throw new ArgumentOutOfRangeException(nameof(i));
+            }
+
+            if (j < 0 || j > 31)
+            {
+                throw new ArgumentOutOfRangeException(nameof(j));
+            }
+
+            if (i > j)
+            {
+                throw new ArgumentException("Thrown when i is more than j.");
+            }
+
+            var binaryString = string.Empty;
+            int result = 0;
+
+            for (int x = 31; x >= 0; x--)
+            {
+                if (x >= i && x <= j)
+                {
+                    binaryString += (char)(((sourceNumber & (1 << ((x - i) % 32))) >> ((x - i) % 32)) | 48);
+                }
+                else
+                {
+                    binaryString += (char)(((destinationNumber & (1 << (x % 32))) >> (x % 32)) | 48);
+                }
+            }
+
+            for (int x = 0; x < binaryString.Length; x++)
+            {
+                result += (binaryString[^(x + 1)] & 1) << (x % 32);
+            }
+
+            return result;
         }
     }
 }
